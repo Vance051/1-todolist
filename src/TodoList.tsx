e82c43511fc035b001832a1c62ee1e4ae8eee678
IvanVance <vance051@gmail.com>
2023-03-13 23:14:52 +0300

    Реаилзовали добавление новой таски в стейт.

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,14 +1,19 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import {FilterValuesType, TaskType} from "./App";
 
 
 type TodoListPropsType = {
     title: string,
     tasks: Array<TaskType>
-    removeTasks: (taskId: number) => void
+    removeTasks: (taskId: string) => void
     changeTodoListFilter: (filter: FilterValuesType) => void
+    addTask: (title:string) => void
 }
+
+
 const TodoList: FC<TodoListPropsType> = (props) => {
+    const [newTitle,setNewTitle] = useState('')
+
     let isAllTaskIsMotDone = true;
     for (let i = 0; i < props.tasks.length; i++) {
         if (props.tasks[i].isDone) {
@@ -19,20 +24,25 @@ const TodoList: FC<TodoListPropsType> = (props) => {
     const todoClasses = isAllTaskIsMotDone ? "todolist-empty" : "todolist";
 
     const todoListItems: Array<JSX.Element> = props.tasks.map((task) => {
-        return (<li>
-            <input type="checkbox" checked={task.isDone}/> <span>{task.title}</span>
+        return (<li key={task.id}>
+            <input  type="checkbox" checked={task.isDone}/> <span>{task.title}</span>
             <button onClick={() =>
                 (props.removeTasks(task.id))}>Delete
             </button>
         </li>)
     }) // Array<JSX.Element>
 
+
     return (
         <div className={todoClasses}>
             <h3>{props.title}</h3>
             <div>
-                <input/>
-                <button>Add new Task</button>
+                <input value={newTitle}  onChange={(event)=>setNewTitle(event.currentTarget.value)}/>
+                <button onClick={() => {
+                    props.addTask(newTitle)
+                    setNewTitle('')
+                }}>Add new Task
+                </button>
             </div>
             <ul>
                 {todoListItems}
