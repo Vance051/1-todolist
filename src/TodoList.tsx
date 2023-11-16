cdf54fc33321d0e0abd3493356b2a9ad08338115
IvanVance <vance051@gmail.com>
2023-11-16 23:44:54 +0300

    Lesson1 - деструктурировал пропсы в todoList

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,30 +1,26 @@
-import React from "react";
+import React, {FC} from "react";
+import {PropsTodoList} from "./App";
 
-export type TaskType = {
-    id: number
-title:string
-isDone:boolean}
-type PropsTodoList = {
-    title: string
-    tasks: TaskType[]
-}
-export const TodoList = (props: PropsTodoList) => {
-    return (<div>
-        <h3>{props.title}</h3>
+
+export const TodoList: FC<PropsTodoList> = (
+    {todoTitle, tasks}) => {
+    // const {todoTitle, tasks} = props
+    return (
         <div>
-            <input/>
-            <button>+</button>
-        </div>
-        <ul>
-            {props.tasks.map((task)=>(<li><input type="checkbox" checked={task.isDone}/> <span>{task.title}</span></li>))}
-            <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-            <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-            <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
-                   </ul>
-        <div>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
-        </div>
-    </div>)
+            <h3>{todoTitle}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {tasks.map((task) => (
+                    <li><input type="checkbox" checked={task.isDone}/> <span>{task.title}</span></li>))}
+
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>)
 }
