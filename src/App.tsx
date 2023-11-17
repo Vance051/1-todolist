31cc34a545ee2bc7615fb7d3929cc98ee95c9bcb
IvanVance <vance051@gmail.com>
2023-11-18 00:02:36 +0300

    Lesson1/microtask - сделал работающие кнопки

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
 import React from 'react';
 import './App.css';
-import {TaskType, TodoList} from "./TodoList";
+import {TodoList} from "./Components/TodoList";
 
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+export type PropsTodoList = {
+    todoTitle: string
+    tasks: TaskType[]
+}
 
 function App() {
-    let task1:TaskType[] = [
-        {id:1, title:'css', isDone:true},
-        {id:2, title:'Js', isDone:true},
-        {id:3, title:'React', isDone:false},
+    //BLL
+    const todoTitle1: string = 'What to learn'
+    const todoTitle2: string = 'What to do'
+    const task1: TaskType[] = [
+        {id: 1, title: 'css', isDone: true},
+        {id: 2, title: 'Js', isDone: true},
+        {id: 3, title: 'React', isDone: false},
     ]
-    let task2:TaskType[] = [
-        {id:1, title:'Reeeead', isDone:true},
-        {id:2, title:'Ruun', isDone:true},
-        {id:3, title:'Sleep', isDone:false},
+    const task2: TaskType[] = [
+        {id: 1, title: 'Read', isDone: true},
+        {id: 2, title: 'Run', isDone: true},
+        {id: 3, title: 'Sleep', isDone: false},
     ]
+
+    //UI---------------------------------
     return (
         <div className="App">
-            <TodoList title='WHat to leeeeern' tasks={task1}/>
-            <TodoList title='WHat to doooooo' tasks={task2}/>
+            <TodoList
+                todoTitle={todoTitle1}
+                tasks={task1}/>
+            <TodoList
+                todoTitle={todoTitle2}
+                tasks={task2}/>
         </div>
     );
 }
---
+++ src/Components/TodoList.tsx
@@ -0,0 +1,40 @@
+import React, {FC} from "react";
+import {PropsTodoList} from "../App";
+import Button from "./Button";
+
+
+export const TodoList: FC<PropsTodoList> = (
+    {todoTitle, tasks}) => {
+
+    // const {todoTitle, tasks} = props
+    const ButtonFoo = (name: string) => {
+        console.log(`Its ${name}  Button`)
+    }
+
+    return (
+        <div>
+            <h3>{todoTitle}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {tasks.map((task) => (
+                    <li><input type="checkbox" checked={task.isDone}/> <span>{task.title}</span></li>))}
+
+            </ul>
+            <div>
+                {/*<button onClick={(event: React.MouseEvent<HTMLButtonElement>) => onClickHandler('All')}>All</button>*/}
+                {/*<button onClick={(event: React.MouseEvent<HTMLButtonElement>) => onClickHandler('Active')}>Active*/}
+                {/*</button>*/}
+                {/*<button*/}
+                {/*    onClick={(event: React.MouseEvent<HTMLButtonElement>) => onClickHandler('Completed')}>Completed*/}
+                {/*</button>*/}
+
+                <Button callBack={() => ButtonFoo('All')} name={'All'}/>
+                <Button callBack={() => ButtonFoo("Active")} name={'Active'}/>
+                <Button callBack={() => ButtonFoo('Completed')} name={'Completed'}/>
+
+            </div>
+        </div>)
+}
